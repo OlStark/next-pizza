50584a3ceb04a2f0a9dd86cf7dcd07667e05067b
Oleg <oleg.starkov@neuro-city.ru>
2025-02-07 15:46:49 +0300

    fix: добавил хедер

---
+++ components/ui/index.ts
@@ -0,0 +1,9 @@
+export  { Button } from "./button";
+export { Skeleton } from "./skeleton";
+export { Input } from "./input";
+export { Select } from "./select";
+export { Slider } from "./slider";
+export { Checkbox } from "./checkbox";
+export { Popover } from "./popover";
+export { Drawer } from "./drawer";
+export {Dialog} from "./dialog"
