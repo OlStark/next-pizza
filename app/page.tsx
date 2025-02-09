080bad94fc9766954a41ed83d9f27cc8da47432a
OLStark <78405223+starkov-sys@users.noreply.github.com>
2025-02-09 13:34:11 +0300

    fix: добавил заголовки разделов

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import {Button } from "@/components/ui/button"
+import { Container, Title } from "@/components/shared";
+import { Categories } from "@/components/shared";
 
 export default function Home() {
   return (
     <>
-    <h1>Home</h1>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold"/>
+        <Categories/>
+      </Container>
     </>
   );
 }
